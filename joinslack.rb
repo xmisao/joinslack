ac09cbb5694f342ab6524cf02ed582c2b82229fc
xmisao <mail@xmisao.com>
2016-07-11 17:47:27 +0900

    support serve option

--- joinslack.rb
+++ joinslack.rb
@@ -2,12 +2,18 @@ require 'net/http'
 require 'pp'
 require 'json'
 require 'optparse'
+require 'webrick'
+require 'erb'
 
 class InviteResult
   attr_accessor :response
 
   def initialize(response)
     @response = response
+  end
+
+  def succeeded?
+    ok? == true
   end
 
   def ok?
@@ -23,7 +29,7 @@ class InviteResult
   end
 
   def inspect
-    fields = {:status => status, :ok? => ok?, :error => error}.to_a.map{|elm| "#{elm[0]}=#{elm[1].inspect}"}.join(', ')
+    fields = {:succeeded? => succeeded?, :status => status, :ok? => ok?, :error => error}.to_a.map{|elm| "#{elm[0]}=#{elm[1].inspect}"}.join(', ')
     "<#{self.class}: #{fields}>"
   end
 end
@@ -40,6 +46,33 @@ def invite(team, email, token)
   InviteResult.new(Net::HTTP.post_form(URI.parse(url), data))
 end
 
+def start_server(team, token)
+  srv = WEBrick::HTTPServer.new({
+    :BindAddress => '0.0.0.0',
+    :Port => 8080})
+
+  path = File.expand_path(File.dirname($0)) + '/htdocs/'
+
+  srv.mount_proc('/'){|req, res|
+    erb = ERB.new(open(path + 'index.erb'){|f| f.read})
+    res.body << erb.result(binding)
+  }
+
+  srv.mount_proc('/invite'){|req, res|
+    email = req.query['email']
+    result = invite(team, email, token)
+
+    message = result.succeeded? ? "Succeeded to invite #{email} !" : "Failed to invite #{email} ..."
+
+    erb = ERB.new(open(path + 'invite.erb'){|f| f.read})
+    res.body << erb.result(binding)
+  }
+
+  Signal.trap(:INT){ srv.shutdown }
+
+  srv.start
+end
+
 OPTS = {}
 
 opt = OptionParser.new
@@ -51,7 +84,7 @@ opt.on('--serve'){|v| OPTS[:serve] = true}
 opt.parse(ARGV)
 
 if OPTS[:serve]
-  raise 'not implemented'
+  start_server(OPTS[:team], OPTS[:token])
 else
   result = invite(OPTS[:team], OPTS[:email], OPTS[:token])
   p result
